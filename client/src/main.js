cd56651c2007312332265de8b8057f7044ab3aeb
dcqxby <1711708217@qq.com>
2023-06-26 22:02:01 +0800

    update element-plus version

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import installElementPlus from './plugins/element'
+import ElementPlus from "element-plus";
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
-installElementPlus(app)
-app.use(store).use(router).mount('#app')
+app.use(store).use(router).use(ElementPlus).mount('#app')
