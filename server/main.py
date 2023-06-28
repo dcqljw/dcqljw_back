f773a264cd6d42da014b3fd94d34d317c6adf090
dcqljw <1711708217@qq.com>
2023-06-28 09:30:29 +0800

    关闭docs redoc

--- server/main.py
+++ server/main.py
@@ -9,17 +9,7 @@ from app.home import api as home_api
 from fastapi import Request
 
 models.Base.metadata.create_all(bind=engine)
-app = FastAPI()
-
-# @app.middleware('http')
-# async def header_auth(request: Request, call_next):
-#     dcq = request.headers.get('dcq', None)
-#     if dcq != "5d9c9e023a33a510e7382393e7286d59":
-#         return JSONResponse(status_code=404, content={"msg": "error"})
-#     response = await call_next(request)
-#     print(response)
-#     return response
-
+app = FastAPI(docs_url=None, redoc_url=None)
 
 app.add_middleware(
     CORSMiddleware,
