4030d31ed6b5b4c242655f92bd0c02c73fbd1f92
dcqljw <1711708217@qq.com>
2023-06-28 22:40:46 +0800

    update 算命不可信

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,12 +1,17 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 import IndexView from '../views/IndexView.vue'
+import FortuneTellerView from "@/views/FortuneTellerView.vue";
 
 const routes = [
     {
         path: '/',
         name: 'index',
         component: IndexView
-    },
+    }, {
+        path: '/FortuneTeller',
+        name: 'FortuneTellerView',
+        component: FortuneTellerView
+    }
 ]
 
 const router = createRouter({
